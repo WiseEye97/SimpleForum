cb1265ef4047b057f09de89bc093d6bfc251ed2c
WiseEyeKOSO <36448457+WiseEyeKOSO@users.noreply.github.com>
2019-04-21 13:33:55 +0200

    cleaning blog writer

--- src/Client/WriteBlog.fs
+++ src/Client/WriteBlog.fs
@@ -3,12 +3,10 @@ module WriteBlog
 open Elmish
 open ViewUtil
 open Fable.Core.JsInterop
-open Fable.Import.Browser
 open PromiseSender
 open Shared.ClientMessages
 open Shared.ServerMessages
 open Thoth.Json
-open Fable.Import.RemoteDev
 open Decoders.userErrosDecoder
 
 type DropDownState = {
@@ -26,56 +24,84 @@ type Model = {
     newCategory : string
 }
 
-type Msg = 
-    | TextChanged of string
-    | TypeChanged of TextType
+type ServerMessages = 
     | CategoriesFethced of string []
     | CantFetchCategories of exn
+    | CategoryResp of AddCategoryResponse*string
+    | CategoryNotAdded of exn
+    | ArticleAdded
+
+type InfoMessages = 
     | TitleChanged of string
     | ChangeCategory of string
     | NewCategoryChanged of string
-    | CategoryResp of AddCategoryResponse*string
-    | CategoryNotAdded of exn
     | AddCategory
     | ToogleDpd
+
+type FormBodyMessages =
+    | TextChanged of string
+    | TypeChanged of TextType  
     | AddSection
     | NewSection
     | AddArticle
-    | ArticleAdded
+     
+
+type Msg =
+    | InfoMessages of InfoMessages
+    | ServerMessages of ServerMessages
+    | FormBodyMessages of FormBodyMessages
+    
 
 let init()  =
-    {newCategory = "";dropD = {isActive = false;category = None;allCategories = []};title = "";sections = [];currentBuffer = None},sendGet<string [],Msg> (Decode.array Decode.string) "/api/getCategories" CategoriesFethced CantFetchCategories
+    {newCategory = "";dropD = {isActive = false;category = None;allCategories = []};title = "";sections = [];currentBuffer = None},sendGet<string [],Msg> (Decode.array Decode.string) "/api/getCategories" (CategoriesFethced >> ServerMessages) (CantFetchCategories >> ServerMessages) 
 
 let update (msg : Msg) (model:Model) =
+
+    let serverUpdate (msg : ServerMessages) (model:Model) =
+        match msg,model with
+        | CategoryResp ({state = true},nc),_ ->
+            {model with dropD = {model.dropD with allCategories = nc :: model.dropD.allCategories }},Cmd.none
+        | CategoriesFethced cat,_ ->
+            {model with dropD = {model.dropD with allCategories = List.ofArray cat}},Cmd.none
+        | _ -> 
+            model,Cmd.none
+
+    let infoUpdate (msg : InfoMessages) (model:Model) =
+        match msg,model with
+        | NewCategoryChanged s,_ ->
+            {model with newCategory = s},Cmd.none
+        | AddCategory,{newCategory = nc} ->
+            {model with newCategory = ""},sendGet<AddCategoryResponse,Msg> newCategoryDecoder (sprintf "/api/addCategory?cat=%s" nc) (fun r -> CategoryResp (r,nc) |> ServerMessages) (CategoryNotAdded >> ServerMessages) 
+        | ChangeCategory s,_ ->
+            {model with dropD = {model.dropD with category = Some s}},Cmd.none
+        | ToogleDpd,_ ->
+            {model with dropD = {model.dropD with isActive = not model.dropD.isActive}},Cmd.none
+        | TitleChanged s,_ ->
+            {model with title = s},Cmd.none
+
+    let formUpdate (msg : FormBodyMessages) (model:Model) =
+        match msg,model with
+        | TextChanged s,{currentBuffer = Some ({content = _} as y)} ->
+            {model with currentBuffer = Some {y with content = s}},Cmd.none
+        | TypeChanged TextType.Normal,{currentBuffer = Some {content = x}} ->
+            {model with currentBuffer = Some {content = x;tp = TextType.Normal}},Cmd.none
+        | TypeChanged TextType.Code,{currentBuffer = Some {content = x}} ->
+            {model with currentBuffer = Some {content = x;tp = TextType.Code}},Cmd.none
+        | AddSection,{sections = ss; currentBuffer = Some x} ->
+            {model with sections = x :: ss;currentBuffer = None},Cmd.none
+        | NewSection,{currentBuffer = None} ->
+            {model with currentBuffer = Some {content = "";tp = TextType.Normal}}, Cmd.none 
+        | AddArticle,{dropD = {category = Some c}} ->
+            {model with currentBuffer = None;sections = []},sendGenericJson<string,Msg> ({NewBlog.category = c;NewBlog.topic = model.title;NewBlog.sections = model.sections}) 1 (Decode.string) "/api/sendBlog" (fun _ -> ArticleAdded |> ServerMessages) (fun _ -> ArticleAdded |> ServerMessages)
+
     match msg,model with
-    | CategoryResp ({state = true},nc),_ ->
-        {model with dropD = {model.dropD with allCategories = nc :: model.dropD.allCategories }},Cmd.none
-    | NewCategoryChanged s,_ ->
-        {model with newCategory = s},Cmd.none
-    | AddCategory,{newCategory = nc} ->
-        {model with newCategory = ""},sendGet<AddCategoryResponse,Msg> newCategoryDecoder (sprintf "/api/addCategory?cat=%s" nc) (fun r -> CategoryResp (r,nc)) CategoryNotAdded
-    | ChangeCategory s,_ ->
-        {model with dropD = {model.dropD with category = Some s}},Cmd.none
-    | ToogleDpd,_ ->
-        {model with dropD = {model.dropD with isActive = not model.dropD.isActive}},Cmd.none
-    | TitleChanged s,_ ->
-        {model with title = s},Cmd.none
-    | CategoriesFethced cat,_ ->
-        {model with dropD = {model.dropD with allCategories = List.ofArray cat}},Cmd.none
-    | TextChanged s,{currentBuffer = Some ({content = _} as y)} ->
-        {model with currentBuffer = Some {y with content = s}},Cmd.none
-    | TypeChanged TextType.Normal,{currentBuffer = Some {content = x}} ->
-        {model with currentBuffer = Some {content = x;tp = TextType.Normal}},Cmd.none
-    | TypeChanged TextType.Code,{currentBuffer = Some {content = x}} ->
-        {model with currentBuffer = Some {content = x;tp = TextType.Code}},Cmd.none
-    | AddSection,{sections = ss; currentBuffer = Some x} ->
-        {model with sections = x :: ss;currentBuffer = None},Cmd.none
-    | NewSection,{currentBuffer = None} ->
-        {model with currentBuffer = Some {content = "";tp = TextType.Normal}}, Cmd.none 
-    | AddArticle,{dropD = {category = Some c}} ->
-        {model with currentBuffer = None;sections = []},sendGenericJson<string,Msg> ({NewBlog.category = c;NewBlog.topic = model.title;NewBlog.sections = model.sections}) 1 (Decode.string) "/api/sendBlog" (fun _ -> ArticleAdded) (fun _ -> ArticleAdded)  
-    | _ ->
-        model,[]
+    | ServerMessages m,md ->
+        serverUpdate m md
+    | InfoMessages m,md ->
+        infoUpdate m md
+    | FormBodyMessages m,md ->
+        formUpdate m md
+
 
 let view  (model : Model) (dispatch : Msg -> unit) =
     let sections = 
@@ -96,23 +122,23 @@ let view  (model : Model) (dispatch : Msg -> unit) =
                     for dpd in model.dropD.allCategories ->
                         let current = model.dropD.category
                         {
-                            onClick = (fun _ -> dpd |> ChangeCategory |> dispatch)
+                            onClick = (fun _ -> dpd |> ChangeCategory |> InfoMessages |> dispatch)
                             name = dpd
                             isActive = current.IsSome && current.Value = dpd
                         }
                 ]
                 desc = "Select blog category"
                 isActive = model.dropD.isActive
-                onExpand = (fun _ -> dispatch ToogleDpd)
+                onExpand = (fun _ -> ToogleDpd |> InfoMessages |> dispatch)
             }
             InpWithButton {
                 placeholder = PlaceHolder "Add new Category"
                 name = "Add"
-                onChange = (fun s -> s |> NewCategoryChanged |> dispatch)
-                onClick = (fun _ -> dispatch AddCategory)
+                onChange = (NewCategoryChanged >> InfoMessages >> dispatch)
+                onClick = (fun _ -> AddCategory |> InfoMessages |> dispatch)
             }
             TextInput {
-                onchange = (fun s -> s |> TitleChanged |> dispatch)
+                onchange = (TitleChanged >> InfoMessages >> dispatch)
                 labeltext = LabelText "Title:"
                 icon = None
                 isPassword = false
@@ -127,34 +153,32 @@ let view  (model : Model) (dispatch : Msg -> unit) =
             [
                 
                 TextArea {
-                    onChange = (fun s ->
-                        s |> TextChanged |> dispatch
-                    )
+                    onChange = TextChanged >> FormBodyMessages >> dispatch
                 }
 
                 RadioInput {
                     name = RadioName "CodeType"
                     values = [
-                        {isSelected = x.tp = Code;value = "Code";onSelect = (fun _ -> Code |> TypeChanged |> dispatch)}
-                        {isSelected = x.tp = TextType.Normal;value = "Normal";onSelect = (fun _ -> TextType.Normal |> TypeChanged |> dispatch)}
+                        {isSelected = x.tp = Code;value = "Code";onSelect = (fun _ -> Code |> TypeChanged |> FormBodyMessages |> dispatch)}
+                        {isSelected = x.tp = TextType.Normal;value = "Normal";onSelect = (fun _ -> Normal |> TypeChanged |> FormBodyMessages |> dispatch)}
                     ]
                 }
 
                 ButtonWithIcon {
                     iconName = IconType "fas fa-check"
-                    onClick = (fun _ -> dispatch AddSection)
+                    onClick = (fun _ -> AddSection |> FormBodyMessages |> dispatch)
                 }
             ]
         | {currentBuffer = None} ->
             [
                 ButtonWithIcon {
                     iconName = IconType "fas fa-plus"
-                    onClick = (fun _ -> dispatch NewSection)
+                    onClick = (fun _ -> NewSection |> FormBodyMessages |> dispatch)
                 }
             ]
         |> Seq.map (fun x -> x,None)
 
     let p1 = Seq.concat [info;p1]
 
-    renderForm p1 (fun _ -> dispatch AddArticle) false
+    renderForm p1 (fun _ -> AddArticle |> FormBodyMessages |> dispatch) false
     |> BlogParser.renderBlogWriter sections
